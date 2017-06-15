2dcf2edd41e4a26c0aeea6203ccc1818a9f07b36
Václav Zeman <prozeman@gmail.com>
2017-06-15 15:16:56 +0200

    Docker compose added

--- docker/start.sh
+++ docker/start.sh
@@ -13,6 +13,7 @@ PARAMETERS=/root/hSVM3/parameters-hSVM3.txt
 STI=1
 LANGUAGE=
 VERSION=
+LHD_SERVER=
 
 for i in "$@"
 do
@@ -23,6 +24,10 @@ case $i in
     ;;
     -l=*)
     LANGUAGE="${i#*=}"
+    shift
+    ;;
+    -lhd=*)
+    LHD_SERVER="${i#*=}"
     shift
     ;;
     *)
@@ -53,6 +58,8 @@ cp $PARAMETERS_TEMPLATE $PARAMETERS
 sed -i -- "s/_LANG_/$LANGUAGE/g" $PARAMETERS
 sed -i -- "s/_VERSION_/$VERSION/g" $PARAMETERS
 
+LHD_IS_READY=0
+
 while IFS='|' read -r key value
 do
 value="$(echo -e "${value}" | sed -e 's/[[:space:]]*$//')"
@@ -60,6 +67,18 @@ if [[ "$key" == "dataset_to_type" || "$key" == "DBpedia_ontology" || "$key" == "
   dexists $value
 fi
 if [[ "$key" == "sti_types_dataset" || "$key" == "sti_inference_debug" ]]; then
+  if [[ -n "$LHD_SERVER" ]]; then
+    while [[ $LHD_IS_READY == 0 ]]; do
+      sleep 1m
+      ping -c 1 "$LHD_SERVER"
+      if [[ $? -gt 0 ]]; then
+        LHD_IS_READY=1
+      fi
+    done
+  fi
+  if [[ -n "$LHD_SERVER" && "$key" == "sti_types_dataset" && LHD_IS_READY == 1 ]]; then
+    gzip "$value" 
+  fi
   if [ ! -f "$value" ]; then
     STI=0
   fi
@@ -74,5 +93,5 @@ fi
 cd /root/hSVM3
 
 mvn exec:java -Dexec.mainClass="cz.vse.swoe.linkedtv.hsvm.hSVMrun"
-
-#mv res/hSVMSTI.nt.gz /root/data/datasets
+       
+mv res/hSVMSTI.nt.gz /root/data/datasets
